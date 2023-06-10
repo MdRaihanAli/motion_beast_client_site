e345a089711ea4a88b3e679fb0b9dfb493b7d90b
MdRaihanAli <raihanraju43@gmail.com>
2023-06-10 22:33:38 +0400

    Approve deny system add

--- src/Components/AllClass/AllClass.jsx
+++ src/Components/AllClass/AllClass.jsx
@@ -1,33 +1,66 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import useClasses from '../../Hooks/useClasses'
 import Table from 'react-bootstrap/Table';
 import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 function AllClass() {
     const [allClasses, refetch] = useClasses()
     console.log(allClasses);
 
-    const handelAprove=(id)=>{
-        fetch(`${import.meta.env.VITE_link}/class/${id}`,{
-            method:'PATCH',
-            headers: {'content-type':'application/json'},
-            body: JSON.stringify({role: "approve"})
+    const handelAprove = (id, text) => {
+        fetch(`${import.meta.env.VITE_link}/class/${id}`, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ role: text, })
         })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            toast("Wow so easy!")
-            refetch()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                toast("Class sturus is Updated",)
+                refetch()
+            })
+    }
+
+
+    //  modal
+    const [defaultId, setdefaultId] = useState(null)
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = (id) => {
+        setdefaultId(id)
+        setShow(true)
+    };
+
+    const feedback = useRef()
+    const handelFidBack = () => {
+        const textData = feedback.current.value
+        // console.log(defaultId, textData);
+
+        fetch(`${import.meta.env.VITE_link}/class/${defaultId}`, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({role: "deny", feedback : textData })
         })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                toast("Class sturus is Updated",)
+                refetch()
+                handleClose()
+            })
     }
 
 
 
-
-  return (
-    <div className='container'>
-            <h1 className='fw-bold my-4 text-center'>All  <span className="text-success">Class</span> </h1>
+    return (
+        <div className='container'>
+            <h1 className='fw-bold my-4 text-center'>Manage <span className="text-success">All  Class</span> </h1>
             <Table striped bordered hover>
                 <thead>
                     <tr>
@@ -54,18 +87,37 @@ function AllClass() {
                             <td>{clas.range}</td>
                             <td>{clas.price}</td>
                             <td>{clas.role}</td>
-                            <td> 
-                                <button disabled={clas.role == 'approve'} onClick={()=>handelAprove(clas._id)} className='btn btn-success btn-sm w-100'>Approve</button>
-                                <button className='btn btn-danger btn-sm  w-100 my-2'>Deny </button>
-                                <button className='btn btn-secondary btn-sm  w-100'>feedback</button>
-                                </td>
+                            <td>
+                                <button disabled={clas.role == 'approve'} onClick={() => handelAprove(clas._id, "approve")} className='btn btn-success btn-sm w-100'>Approve</button>
+                                <button disabled={clas.role == 'deny'} onClick={() => handelAprove(clas._id, "deny")} className='btn btn-danger btn-sm  w-100 my-2'>Deny </button>
+                                <button disabled={clas.role == 'approve' || clas.role == 'pending' } onClick={() => handleShow(clas._id)} className='btn btn-secondary btn-sm  w-100'>feedback</button>
+                            </td>
                         </tr>)
                     }
 
                 </tbody>
             </Table>
+
+
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <input type="text" name='feedback' ref={feedback} />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="success" onClick={handelFidBack}>
+                        Save Feedback
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
-  )
+    )
 }
 
 export default AllClass
